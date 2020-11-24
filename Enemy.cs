9c40b00752896e4f1f641bbec4c42034187c96fd
Søren Jørgensen <skmj@hotmail.dk>
2020-11-24 18:44:50 +0100

    Auto stash before merge of "master" and "origin/master"

--- Enemy.cs
+++ Enemy.cs
@@ -1,10 +1,57 @@
-﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Adventure_man
 {
-    class Enemy
-{
-}
+     class Enemy : Moveable
+    {
+        private int health;
+        private bool isAlive;
+
+
+        public Enemy()
+        {
+
+            speed = 100;
+            effect = SpriteEffects.None;
+            color = Color.Red;
+            scale = 5f;
+        }
+
+
+        public override void LoadContentMoveable(ContentManager content)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, Color.White);
+        }
+
+        public override void UpdateMoveable(GameTime gameTime)
+        {
+            Move(gameTime);
+        }
+
+        public void Attack()
+        {
+
+        }
+
+        public void TakeDamage()
+        {
+
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
