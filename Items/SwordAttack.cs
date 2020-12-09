bcb81d7b5de5504ca59e87e860d91297e91a461a
Magnus Starup <mavi.nexu@gmail.com>
2020-12-09 12:15:19 +0100

    Using OnCollision in sword attacks, changed MenuFont to arial

--- Items/SwordAttack.cs
+++ Items/SwordAttack.cs
@@ -13,15 +13,13 @@ namespace Adventure_man
         private MoveableGameObject friendly;
         private Vector2 stab;
 
-
-
         public SwordAttack(Texture2D sprite, Vector2 position, int damage, MoveableGameObject friendly, GameWorld.Direction direction)
         {
             dir = direction;
             this.friendly = friendly;
             Location = position;
             Sprite = sprite;
-            Size = new Vector2(Sprite.Width , Sprite.Height);
+            Size = new Vector2(Sprite.Width, Sprite.Height);
             this.damage = damage;
             FlipSprite();
         }
@@ -30,45 +28,52 @@ namespace Adventure_man
         {
         }
 
-
-
         public override void Update()
         {
-
             // Ras - Changes swords velocity increasingly, and removes it if too far away from the player,
             // missing: rotate, size, effect ?
-                if (stab.X > origin.X +10 || stab.X < origin.X - 10)
+            if (stab.X > origin.X + 10 || stab.X < origin.X - 10)
             {
-                
                 Destroy(this);
-
             }
             else
             {
-                // 
+                //
                 stab.X = stab.X + (int)dir;
 
                 velocity = stab + World.Player.velocity;
             }
 
-
             // Ras - Edited version of arrow, sword is not removed upon collision but dmg is stil taken
-            foreach (var other in Program.AdventureMan.CurrentWorld.Objects)
-                if (HitBox.Intersects(other.HitBox))
-                {
-                    if (other is Enemy && other != friendly)
-                    {
-                        ((Enemy)other).TakeDamage(damage);
-                    }
-                    if (other is Player && other != friendly)
-                    {
-                        ((Player)other).TakeDamage(damage);
-                    }
+            //foreach (var other in Program.AdventureMan.CurrentWorld.Objects)
+            //    if (HitBox.Intersects(other.HitBox))
+            //    {
+            //        if (other is Enemy && other != friendly)
+            //        {
+            //            ((Enemy)other).TakeDamage(damage);
+            //        }
+            //        if (other is Player && other != friendly)
+            //        {
+            //            ((Player)other).TakeDamage(damage);
+            //        }
 
-                }
+            //    }
 
             base.Update();
+        }
 
+        public override void OnCollision(GameObject collisionTarget)
+        {
+            if (collisionTarget is Enemy && collisionTarget != friendly)
+            {
+                ((Enemy)collisionTarget).TakeDamage(damage);
+            }
+            if (collisionTarget is Player && collisionTarget != friendly)
+            {
+                ((Player)collisionTarget).TakeDamage(damage);
+            }
+
+            base.OnCollision(collisionTarget);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
--- Items/SwordAttackEnemy.cs
+++ Items/SwordAttackEnemy.cs
@@ -7,13 +7,11 @@ using System.Text;
 
 namespace Adventure_man
 {
-    class SwordAttackEnemy : MoveableGameObject
+    internal class SwordAttackEnemy : MoveableGameObject
     {
         private int damage;
         private MoveableGameObject friendly;
         private Vector2 stab;
-
-
 
         public SwordAttackEnemy(Texture2D sprite, Vector2 position, int damage, MoveableGameObject friendly, GameWorld.Direction direction)
         {
@@ -30,41 +28,43 @@ namespace Adventure_man
         {
         }
 
-
-
         public override void Update()
         {
-
             // Ras - Changes swords velocity increasingly, and removes it if too far away from the player,
             // missing: rotate, size, effect ?
             if (stab.X > origin.X + 10 || stab.X < origin.X - 10)
             {
-
                 Destroy(this);
-
             }
             else
             {
-                // 
+                //
                 stab.X = stab.X + (int)dir;
 
                 velocity = stab + World.Player.velocity;
             }
 
-
             // Ras - Edited version of arrow, sword is not removed upon collision but dmg is stil taken
-            foreach (var other in Program.AdventureMan.CurrentWorld.Objects)
-                if (HitBox.Intersects(other.HitBox))
-                {
-                    if (other is Player && other != friendly)
-                    {
-                        ((Player)other).TakeDamage(damage);
-                    }
-
-                }
+            //foreach (var other in Program.AdventureMan.CurrentWorld.Objects)
+            //    if (HitBox.Intersects(other.HitBox))
+            //    {
+            //        if (other is Player && other != friendly)
+            //        {
+            //            ((Player)other).TakeDamage(damage);
+            //        }
+            //    }
 
             base.Update();
+        }
 
+        public override void OnCollision(GameObject collisionTarget)
+        {
+            if (collisionTarget is Player && collisionTarget != friendly)
+            {
+                ((Player)collisionTarget).TakeDamage(damage);
+            }
+
+            base.OnCollision(collisionTarget);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
